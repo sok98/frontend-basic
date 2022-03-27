c335320ed368896934fcc6680eeba18bb76f6a10
Yeseul Seo <sok98@naver.com>
2022-03-27 21:13:35 +0900

    part08. ch01 bundler  parcel

---
+++ part_08/.postcssrc.js
@@ -0,0 +1,22 @@
+//ESM
+// import
+// export
+
+//CommonJs
+// require() // import
+// module.exports  //export
+
+
+// import autoprefixer from 'autoprefixer'
+// const autoprefixer = require('autoprefixer')
+
+// export {
+//   plugins: [
+//     autoprefixer
+//   ]
+// }
+module.exports={
+  plugins: [
+    require('autoprefixer')
+  ]
+}
